a206d746f428dcd6bfa0297689880db9365e78d5
Fantasy Shao <forever.fantasy27@gmail.com>
2016-07-30 22:23:33 +0800

    Skeleton :rocket:

---
+++ src/app.js
@@ -0,0 +1,24 @@
+/**
+ * Module dependencies
+ */
+
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import { Router, Route, hashHistory } from 'react-router'
+
+import Home from './components/Home'
+import Settings from './components/Settings'
+import About from './components/About'
+import Error from './components/Error'
+
+import './app.scss'
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Home} />
+    <Route path="/settings" component={Settings} />
+    <Route path="/about" component={About} />
+    <Route path="*" component={Error} />
+  </Router>,
+  document.getElementById('app')
+)
