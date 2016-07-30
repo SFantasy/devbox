f433604686322a1c46d9dbf5e86e13065cf8cb05
Fantasy Shao <forever.fantasy27@gmail.com>
2016-07-30 22:25:06 +0800

    Add <About /> and <Error />

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Layout from '../Layout'
+
+export default class About extends Component {
+  render () {
+    return (
+      <Layout>
+        <h1>ABOUT</h1>
+      </Layout>
+    )
+  }
+}
---
+++ src/components/Error/index.js
@@ -0,0 +1,13 @@
+// Module dependencies
+import React, { Component } from 'react'
+import Layout from '../Layout'
+
+export default class Error extends Component {
+  render () {
+    return (
+      <Layout>
+        <h1 className='page-title'>ERROR NOT FOUND</h1>
+      </Layout>
+    )
+  }
+}
