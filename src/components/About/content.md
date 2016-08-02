0d1a4de97a6981b67cbf5d384149f16c5d9f8124
Fantasy Shao <forever.fantasy27@gmail.com>
2016-08-02 19:25:31 +0800

    Add about content and update related stylesheet.

---
+++ src/components/About/content.md
@@ -0,0 +1,7 @@
+## Introduction
+
+DevBox is an application for developers.
+
+---
+
+Built with [Electron](http://electron.atom.io) | Fork on [Github](https://github.com/SFantasy/devbox)
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Layout from '../Layout'
+import content from 'html!markdown!./content.md'
 
 export default class About extends Component {
   render () {
     return (
       <Layout>
-        <h1>ABOUT</h1>
+        <h1 className='page-title'>About</h1>
+        <main className='page-md' dangerouslySetInnerHTML={{__html: content}} />
       </Layout>
     )
   }
