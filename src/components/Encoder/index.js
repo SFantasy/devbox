fb28bf0d04b1d6e76930fa26a2d9167120aa7ecd
Fantasy Shao <forever.fantasy27@gmail.com>
2016-08-10 18:57:19 +0800

    Add base64 and md5 in <Encoder />

--- src/components/Encoder/index.js
+++ src/components/Encoder/index.js
@@ -1,10 +1,11 @@
 // Module dependencies
 import React, { Component } from 'react'
 import Layout from '../Layout'
+import crypto from 'crypto'
 
 import './style.scss'
 
-const ENCODER_TYPES = ['base64', 'md5', 'aes', 'des']
+const ENCODER_TYPES = ['base64', 'md5']
 
 export default class Encoder extends Component {
 
@@ -22,7 +23,9 @@ export default class Encoder extends Component {
         <h1 className='page-title'>Encoder</h1>
         <div className='encoder-container'>
           <div className='encoder-area'>
-            <textarea placeholder='Input string to be encrypted' />
+            <textarea
+              placeholder='Input string to be encrypted'
+              ref='input' />
           </div>
           <div className='encoder-actions'>
             <select
@@ -36,14 +39,50 @@ export default class Encoder extends Component {
                 <option key={index} value={index}>{`${type}`}</option>
               )}
             </select>
-            <button className='btn' style={{marginTop:'10px'}}>Encode »</button>
-            <button className='btn' style={{marginTop:'10px'}}>« Decode</button>
+            <button className='btn' style={{marginTop:'10px'}} onClick={this._encode.bind(this)}>Encode »</button>
+            <button className='btn' style={{marginTop:'10px'}} onClick={this._decode.bind(this)}>« Decode</button>
           </div>
           <div className='encoder-area'>
-            <textarea placeholder='Input string to be decrypted' />
+            <textarea placeholder='Input string to be decrypted' ref='output' />
           </div>
         </div>
       </Layout>
     )
+  }
+
+  _encode () {
+    let input_string = this.refs.input.value
+    let { type } = this.state
+    let output = ''
+
+    switch (parseInt(type, 10)) {
+      case 0:
+        output = new Buffer(input_string).toString('base64')
+        break
+      case 1:
+        output = crypto.createHash('md5').update(input_string).digest('hex')
+        break
+      default:
+        alert('Not valid encode type')
+    }
+
+    this.refs.output.value = output
+  }
+
+  _decode () {
+    let output_string = this.refs.output.value
+    let { type } = this.state
+    let decoded_string = ''
+
+    switch (type) {
+      case 0:
+        decoded_string = new Buffer(output_string, 'base64').toString()
+        break
+      default:
+        alert('Not valid decode type')
+    }
+
+    this.refs.input.value = decoded_string
+
   }
 }
