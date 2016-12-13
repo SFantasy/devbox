39a367fd5fd5416aa2e9cf052cca6630138957b8
Fantasy Shao <forever.fantasy27@gmail.com>
2016-12-13 11:22:31 +0800

    add files

---
+++ src/components/Settings/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Layout from '../Layout'
+
+export default class Settings extends Component {
+  render () {
+    return (
+      <Layout>
+        <h1 className='page-title'>Settings</h1>
+      </Layout>
+    )
+  }
+}
