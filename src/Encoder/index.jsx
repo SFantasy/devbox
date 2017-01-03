e577186c799a824ae533e5e3a5be56079d36ce5e
Fantasy Shao <forever.fantasy27@gmail.com>
2017-01-04 00:25:36 +0800

    encoder: add base64

--- src/Encoder/index.jsx
+++ src/Encoder/index.jsx
@@ -15,28 +15,85 @@ export default class Encoder extends Component {
   constructor(props) {
     super(props);
 
+    this.encode = this.encode.bind(this);
+    this.decode = this.decode.bind(this);
+
     this.state = {
       type: ENCODER_TYPES[0],
+      text: '',
     };
   }
 
   render() {
-    const { type } = this.state;
+    const {
+      type,
+      text,
+    } = this.state;
 
     return (
       <div className="encoder">
         <div className="encoder-bar">
           <span>Type: </span>
-          <Select style={{ width: 100 }} value={type}>
+          <Select
+            style={{ width: 100 }}
+            value={type}
+            onChange={(value) => {
+              this.setState({
+                type: value,
+              });
+            }}
+          >
             {ENCODER_TYPES.map((t, index) =>
               <Option key={index} value={t}>{t}</Option>
             )}
           </Select>
-          <Button>Encode</Button>
-          <Button>Decode</Button>
+          <Button onClick={this.encode}>Encode</Button>
+          <Button onClick={this.decode}>Decode</Button>
         </div>
-        <Input type="textarea" />
+        <Input
+          type="textarea"
+          value={text}
+          onChange={(e) => {
+            this.setState({
+              text: e.target.value,
+            });
+          }}
+        />
       </div>
     );
+  }
+
+  encode() {
+    const { type, text } = this.state;
+
+    let encodedText = '';
+
+    switch (type) {
+      case 'base64':
+        encodedText = new Buffer(text).toString('base64');
+        break;
+      default:
+    }
+
+    this.setState({
+      text: encodedText,
+    });
+  }
+
+  decode() {
+    const { type, text } = this.state;
+
+    let decodedText = '';
+
+    switch (type) {
+      case 'base64':
+        decodedText = new Buffer(text, 'base64');
+        break;
+      default:
+    }
+
+    this.setState({
+      text: decodedText,
+    });
   }
 }
