d8670c6ac0dc9bc927f64388890520db51d4b0af
Fantasy Shao <forever.fantasy27@gmail.com>
2017-01-06 00:25:36 +0800

    restool: start

--- src/REST/index.jsx
+++ src/REST/index.jsx
@@ -1,9 +1,80 @@
 import React, { Component } from 'react';
+import {
+  Row,
+  Col,
+  Select,
+  Input,
+  Button,
+  Tabs,
+  Collapse,
+  message,
+} from 'antd';
+
+import './style.scss';
+
+const Option = Select.Option;
+const TabPane = Tabs.TabPane;
+const Panel = Collapse.Panel;
+
+const METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'];
 
 export default class RESTool extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      method: METHODS[0],
+    };
+  }
+
   render() {
+    const {
+      method,
+    } = this.state;
+
     return (
-      <h1>REST tool</h1>
+      <div className="restool">
+        <div className="rest-header">
+          <div className="rest-url">
+            <Select
+              value={method}
+              style={{ width: 100 }}
+              onChange={(value) => {
+                this.setState({
+                  method: value,
+                });
+              }}
+            >
+              {METHODS.map((m, index) => 
+                <Option key={index} value={m}>{m}</Option>
+              )}
+            </Select>
+            <Input placeholder="URL here" style={{ width: 400, marginLeft: 10 }} />
+          </div>
+          <Button type="primary" icon="reload" size="large">Go</Button>
+        </div>
+        <Row gutter={10}>
+          <Col span={10}>
+            <Tabs>
+              <TabPane tab="Request" key="1">
+                <Collapse bordered={false} defaultActiveKey={['1', '2', '3']}>
+                  <Panel header="Headers" key="1"></Panel>
+                  <Panel header="Request Parameters" key="2"></Panel>
+                  <Panel header="Request Body" key="3"></Panel>
+                </Collapse>
+              </TabPane>
+              <TabPane tab="Cookie" key="2"></TabPane>
+            </Tabs>
+          </Col>
+          <Col span={14}>
+            <Tabs>
+              <TabPane tab="Response" key="1">
+                <Input type="textarea" style={{ height: '100%' }} />
+              </TabPane>
+            </Tabs>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
