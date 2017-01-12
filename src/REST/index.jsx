92b3e8ef1ab008f87b0e902eb5079757b284bcf8
Fantasy Shao <forever.fantasy27@gmail.com>
2017-01-13 00:22:35 +0800

    restool: making request with superagent

--- src/REST/index.jsx
+++ src/REST/index.jsx
@@ -1,4 +1,9 @@
+/**
+ * Module dependencies
+ */
+
 import React, { Component } from 'react';
+import request from 'superagent';
 import {
   Row,
   Col,
@@ -20,18 +25,24 @@ const Panel = Collapse.Panel;
 
 const METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'];
 
+const LINK_REGEX = /http(s)?\:\/\/([\w\d.?]+(\/)?)+/;
+
 export default class RESTool extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       method: METHODS[0],
+      url: '',
+      response: '',
     };
   }
 
   render() {
     const {
       method,
+      url,
+      response,
     } = this.state;
 
     return (
@@ -51,11 +62,23 @@ export default class RESTool extends Component {
                 <Option key={index} value={m}>{m}</Option>
               )}
             </Select>
-            <Input placeholder="URL here" style={{ width: 400, marginLeft: 10 }} />
+            <Input
+              placeholder="URL here"
+              style={{ width: 400, marginLeft: 10 }}
+              value={url}
+              onChange={(e) => {
+                this.setState({
+                  url: e.target.value,
+                });
+              }}
+            />
           </div>
-          <Button type="primary" icon="reload" size="large">Go</Button>
+          <Button
+            type="primary" icon="reload" size="large"
+            onClick={this.request.bind(this)}
+          >Go</Button>
         </div>
-        <Row gutter={10}>
+        <Row gutter={10} type="flex" style={{ height: 'calc(100% - 32px)' }}>
           <Col span={10}>
             <Tabs>
               <TabPane tab="Request" key="1">
@@ -81,7 +104,7 @@ export default class RESTool extends Component {
                     />
                   </Panel>
                   <Panel header="Request Body" key="3">
-                    <Input type="textarea" />
+                    <Input type="textarea" autosize={{ minRows: 5 }} />
                   </Panel>
                 </Collapse>
               </TabPane>
@@ -106,12 +129,58 @@ export default class RESTool extends Component {
           <Col span={14}>
             <Tabs>
               <TabPane tab="Response" key="1">
-                <Input type="textarea" style={{ height: '100%' }} />
+                <Button
+                  onClick={() => {
+                    this.setState({
+                      response: '',
+                    });
+                  }}>Clear</Button>
+                <Input
+                  type="textarea"
+                  autosize={{ minRows: 20, maxRows: 30 }}
+                  value={response}
+                  style={{
+                    marginTop: 20,
+                  }}
+                />
               </TabPane>
             </Tabs>
           </Col>
         </Row>
       </div>
     );
+  }
+
+  request() {
+    const {
+      url,
+      method,
+    } = this.state;
+
+    if (!url.length) return message.warn('URL is empty');
+    if (!LINK_REGEX.test(url)) return message.warn('Not valid URL');
+
+    message.info('Start requesting...');
+
+    request(method, url)
+      .end((err, res) => {
+        message.info('Request completed.');
+
+        let showText = '';
+
+        switch(typeof res.body) {
+          case 'string':
+            showText = res.body;
+            break;
+          case 'object':
+            showText = JSON.stringify(res.body, null, '    ');
+            break;
+          default:
+        }
+
+        this.setState({
+          response: showText,
+        });
+      });
   }
 }
